9bc6e86e88408e2ab98f63392676ba362e487f76
SadaafChowdhury <sadaaf.chowdhury@northsouth.edu>
2020-10-01 14:03:23 -0700

    Initial commit

---
+++ index.py
@@ -0,0 +1,14 @@
+# Import the required libraries :
+from tkinter import *
+import random
+import simpleaudio as sa
+
+root = Tk()
+root.configure(bg="#000000")
+root.geometry('+0+0')
+root.iconbitmap("Game.ico")
+root.title("Rock-Paper-Scissor-Lizard-Spock")
+root.resizable(width=False,height=False)
+
+x = 5
+print(x)
